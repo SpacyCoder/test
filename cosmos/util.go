5cc2eeb28e5bdf134658d18fb6dbf6588c85e4e5
Lars Heimdal <sral_h@hotmail.com>
2019-04-24 14:41:11 +0200

    Update util.go

--- cosmos/util.go
+++ cosmos/util.go
@@ -11,6 +11,10 @@ type Coordinate [2]float64
 
 type Coordinates []Coordinate
 
+type Geometry interface {
+	Coords() *Coordinates
+}
+
 // LineString struct defines a line string
 type LineString struct {
 	Type        string      `json:"type"`
@@ -26,4 +30,27 @@ func NewLineString(coords ...Coordinate) *LineString {
 // AddPoint is a helper method for adding point to a LineString
 func (l *LineString) AddPoint(lon, lat float64) {
 	l.Coordinates = append(l.Coordinates, Coordinate{lon, lat})
+}
+
+func (l *LineString) Coords() *Coordinates {
+	return &l.Coordinates
+}
+
+type Polygon struct {
+	Type        string      `json:"type"`
+	Coordinates Coordinates `json:"coordinates"`
+}
+
+// NewPolygon creates a new Polygon struct.
+func NewPolygon(coords ...Coordinate) *Polygon {
+	line := &Polygon{Type: "Polygon", Coordinates: coords}
+	return line
+}
+
+func (p *Polygon) AddPoint(lon, lat float64) {
+	p.Coordinates = append(p.Coordinates, Coordinate{lon, lat})
+}
+
+func (p *Polygon) Coords() *Coordinates {
+	return &p.Coordinates
 }
